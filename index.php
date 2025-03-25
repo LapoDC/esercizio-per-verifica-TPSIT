596a0e11ff6de432a03d51e88cfe95c6b0ea9d5f
lai2-24 <lai2-24@itismeucci.com>
2025-03-25 14:29:44 +0100

    second commit

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ $app->get('/alunni', "AlunniController:index"); //curl http://localhost:8080/alu
 
 $app->get('/alunni/{id}', "AlunniController:show"); //curl http://localhost:8080/alunni/2
 
-$app->post('/alunni', "AlunniController:post"); //curl -X POST http://localhost:8080/alunni -N "Content-Type: application/json" -d '{"nome":"ciccio,"cognome":"bello"}
+$app->post('/alunni', "AlunniController:post"); //curl -X  POST http://localhost:8080/alunni -H "Content-Type: application/json" -d '{"nome":"ciccio,"cognome":"bello"}'
 
-$app->put('/alunni/{id}', "AlunniController:put"); //curl -X PUT http://localhost:8080/alunni -N "Content-Type: application/json" -d '{"nome":"ciccio,"cognome":"bello"}
+$app->put('/alunni/{id}', "AlunniController:put"); //curl -X  PUT http://localhost:8080/alunni/1 -H "Content-Type: application/json" -d '{"nome":"ciccio,"cognome":"bello"}'
 
 $app->delete('/alunni/{id}', "AlunniController:delete"); //curl -X DELETE http://localhost:8080/alunni/2
 
